d94a9020d74160768def656e24aaa4d260447fa9
Brian Lambert <brianlambert@gmail.com>
2025-07-04 17:25:10 -0700

    Initial Commit

---
+++ AwfulMainActorIsolatedExample/AwfulMainActorIsolatedExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AwfulMainActorIsolatedExampleApp.swift
+//  AwfulMainActorIsolatedExample
+//
+//  Created by Brian Lambert on 7/4/25.
+//
+
+import SwiftUI
+
+@main
+struct AwfulMainActorIsolatedExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AwfulMainActorIsolatedExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AwfulMainActorIsolatedExample
+//
+//  Created by Brian Lambert on 7/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
